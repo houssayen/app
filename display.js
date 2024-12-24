86dc1f776e3ed3d7846bffe553e44ce63e647f25
houssayen <houssayen.souissi@gmail.com>
2024-12-24 18:06:00 +0100

    expiration date transformation

--- display.js
+++ display.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    function formatDateTime(dateTime) {
+    function formatDateTimeWithLocation(dateTime) {
 
         const dateObj = new Date(dateTime);
         const dateLocal = dateObj.toLocaleDateString('fr-FR');
@@ -11,7 +11,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
         return `${dateLocal} - ${timeLocal}`;
     }
-    
+
+    function formatDateTime(dateTime) {
+
+        const dateObj = new Date(dateTime);
+        // Formater la date (YYYY-MM-DD) en UTC
+        const dateLocal = dateObj.toISOString().split('T')[0];  // Format : YYYY-MM-DD (UTC)
+
+        // Extraire l'heure et les minutes (HH:mm) en UTC
+        const hours = String(dateObj.getUTCHours()).padStart(2, '0');  // Assure que l'heure est toujours sur 2 chiffres
+        const minutes = String(dateObj.getUTCMinutes()).padStart(2, '0');  // Assure que les minutes sont sur 2 chiffres
+
+        // Retourner la date et l'heure sous le format "YYYY-MM-DD - HH:mm" en UTC
+        return `${dateLocal} - ${hours}:${minutes}`;
+    }
+
     async function afficherProduits() {
         try {
             //const response = await fetch('http://localhost:3000/api/produits', { method: 'GET' });
@@ -23,13 +37,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const produits = await response.json();
             console.log(produits);
+
             const tableBody = document.getElementById('produits-table-body');
             tableBody.innerHTML = '';
 
             produits.forEach(produit => {
                 const row = document.createElement('tr');
                 row.innerHTML = `       
-                    <td>${formatDateTime(produit_created_at)}</td>        
+                    <td>${formatDateTimeWithLocation(produit.created_at)}</td>        
                     <td>${produit.nom_produit}</td>              
                     <td>${`${produit.jours}j`}</td>              
                     <td>${formatDateTime(produit.date_expiration)}</td>              
@@ -37,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 tableBody.appendChild(row);
             });
         } catch (error) {
-            console.error('Erreur lors de l\'affichage des produits :', error.message);
+            console.error('Erreur lors de l\'affichage des produits :', error);
         }
     };
 
